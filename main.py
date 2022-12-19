2a988fe4e42c4f12718d468d810c6fdfaa924086
Matomo Niwano <47988393+matomoniwano@users.noreply.github.com>
2022-12-19 17:14:58 +0900

    Maschine Learning for Category Classiication added

--- main.py
+++ main.py
@@ -13,25 +13,107 @@ from selenium.webdriver.support.ui import Select
 from datetime import date, timedelta, datetime
 import time
 from selenium.webdriver.chrome.options import Options
-import gspread
+#import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+#from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator
+import heapq
+import pickle
 
+
+
+# def textclassification(a):
+#     api_key   = "XGhjqzEtQylcOulQCSae3rgtB0iHau6qa4DlcYxLFNg"
+#     paralleldots.set_api_key( api_key )
+#     #print(a)
+#     print(paralleldots.taxonomy( a ))
+#     return paralleldots.taxonomy( a )
+
+# def category_extractor(text):
+#     api = textclassification(text)
+#     print(api)
+#     apiresult = api["taxonomy"]
+#     apiresult_dic = {}
+#     for i in apiresult:
+#         apiresult_dic[i["tag"]] = float(i["confidence_score"])
+#     category = heapq.nlargest(1, apiresult_dic, key=apiresult_dic.get)
+
+#     return category[0]
+
+def predict_category(text, classifier, tfidf_vectorizer):
+    result = classifier.predict(tfidf_vectorizer.transform([text]))
+    return(result[0])
+    
+
+def NLP_categorizer(txt):
+
+    # Use pickle to load in the pre-trained model.
+    with open('vector.pkl', 'rb') as file:
+        tfidf_vectorizer = pickle.load(file)
+        
+    with open('class.pkl', 'rb') as file:
+        classifier = pickle.load(file)
+
+    return predict_category(txt, classifier, tfidf_vectorizer)
+
+
+
+def process_article(soup, link):
+    temp = []
+    category = soup.find_all('div', class_='col-md-9')
+    title_en = soup.find('h1', class_ = 'node-title').string
+    
+    url = title_en.replace(" ", "-")
+    title_ja = GoogleTranslator(source='en', target='ja').translate(title_en).replace(".", "。").replace("グルジア", "ジョージア")
+
+    body = soup.find_all('div', class_ = 'row bodytext')
+    bodyall = soup.find('div', class_ = 'row bodytext').text
+    index = int(len(bodyall)/2)
+    print(index)
+    #cat = category_extractor(bodyall[0:index])
+    cat = NLP_categorizer(bodyall)
+    #print(body[0])
+
+    #print()
+    #cat = category[0].find_all('img')[0]["src"].replace("/", " ").split()[1]
+
+    for img in body[0].find_all('img'):
+        #print(img["src"])
+        img["src"] = "https://agenda.ge" + img["src"]
+
+    #newbody = str(body[0])
+    for pg in body[0].find_all('p'):
+        if pg.string :
+            pg.string.replace_with(GoogleTranslator(source='en', target='ja').translate(pg.string).replace(".", "。").replace("グルジア", "ジョージア"))
+        else:
+            pg.string = GoogleTranslator(source='en', target='ja').translate(pg.text).replace(".", "。").replace("グルジア", "ジョージア")
+        #newbody = newbody.replace(pg.text, GoogleTranslator(source='en', target='ja').translate(pg.text))
+
+    finalbody = "[blogcard url=" + link + "]"+ str(body[0])
+    print("after process")
+    #Title
+    temp.append(title_ja)
+    #url
+    temp.append(url)
+    #body
+    temp.append(finalbody)
+    #category
+    temp.append(cat)
+    return temp
 
 
 def getcontents(driver):
     contents = []
     driver.get("https://agenda.ge/en/news/news")
-    sleep(3)
-
+    sleep(1)
     #should be 16
-    print("checking here")
-    dates = driver.find_elements_by_xpath("//div[@class='node-teaser-time']")
-    print("dates", len(dates))
-    links = driver.find_elements_by_xpath("//a[@class='node-teaser-title']")
+    print("front page about to get dates")
+    dates = driver.find_elements_by_xpath( "//div[@class='node-teaser-time']")
+    links = driver.find_elements_by_xpath( "//a[@class='node-teaser-title']")
     pages = []
     
+
     # Check dates for today
     for i in range(len(dates)):
         #insert - before d when GCP
@@ -42,47 +124,24 @@ def getcontents(driver):
             pages.append(links[i].get_attribute("href"))
 
     # Done getting all the URLs now moving to each page
-
+    linkref = {}
     for link in pages:
-        temp = []
         response = requests.get(link)
         soup = BeautifulSoup(response.text, 'html.parser')
-        category = soup.find_all('div', class_='col-md-9')
-        title_en = soup.find('h1', class_ = 'node-title').string
-        url = title_en.replace(" ", "-")
-        title_ja = GoogleTranslator(source='en', target='ja').translate(title_en).replace("グルジア", "ジョージア").replace(".", "。")
+        lentext = soup.find('div', class_ = 'row bodytext')
+        linkref[link] = (len(lentext.text))
 
-        body = soup.find_all('div', class_ = 'row bodytext')
 
-        #print(body[0])
+    print(linkref)
 
-        print()
-        cat = category[0].find_all('img')[0]["src"].replace("/", " ").split()[1]
+    top5 = heapq.nlargest(5, linkref, key=linkref.get)
 
-        for img in body[0].find_all('img'):
-            #print(img["src"])
-            img["src"] = "https://agenda.ge" + img["src"]
+    print(top5)
 
-        #newbody = str(body[0])
-        for pg in body[0].find_all('p'):
-            if pg.string :
-                pg.string.replace_with(GoogleTranslator(source='en', target='ja').translate(pg.string).replace("グルジア", "ジョージア").replace(".", "。"))
-            else:
-                pg.string = GoogleTranslator(source='en', target='ja').translate(pg.text).replace("グルジア", "ジョージア").replace(".", "。")
-            #newbody = newbody.replace(pg.text, GoogleTranslator(source='en', target='ja').translate(pg.text))
-
-        finalbody = "[blogcard url=" + link + "]"+ str(body[0])
-
-        #Title
-        temp.append(title_ja)
-        #url
-        temp.append(url)
-        #body
-        temp.append(finalbody)
-        #category
-        temp.append(cat)
-
-        contents.append(temp)
+    for link in top5:
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, 'html.parser')     
+        contents.append(process_article(soup, link))
 
     return contents
 
@@ -105,81 +164,77 @@ def AAG(requests):
     chrome_options.binary_location = os.getcwd() + "/headless-chromium"    
     driver = webdriver.Chrome(os.getcwd() + "/chromedriver",chrome_options=chrome_options)
 
-
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
+    # driver.set_window_size(1280, 1696)
     contents = getcontents(driver)
-    driver.set_window_size(1024, 600)
-    driver.maximize_window()
-    # Scraping
-
     driver.get("https://00.ge/wp-admin/index.php")
     sleep(1)
     login = driver.find_element_by_id("user_login")
     login.send_keys("matomo.niwano@gmail.com")
     pwd = driver.find_element_by_id("user_pass")
     pwd.send_keys("@32jG5qqAyk4I#WuV3IOtN*q")
-    submit = driver.find_element_by_id("wp-submit")
+    submit = driver.find_element_by_id( "wp-submit")
     submit.click()
-    sleep(1)
-    print("content: ", len(contents))
+
     for content in contents:
-
+        print(content[1])
         driver.get("https://00.ge/wp-admin/post-new.php?post_type=custom&customize_changeset_uuid=")
-
+        driver.save_screenshot('yahooooo.png')
         sleep(2)
         title = driver.find_element_by_name("post_title")
         title.send_keys(content[0])
-        sleep(1)
-
-
-        body = driver.find_element_by_xpath("//textarea[@id='content']")
+        body = driver.find_element_by_xpath( "//textarea[@id='content']")
         body.click()
-        sleep(2)
-        body.send_keys(content[2])
-
-        sleep(3)
-        driver.execute_script("window.scrollTo(0, 220)")
         sleep(1)
-        slug = driver.find_element_by_xpath("//button[@class='edit-slug button button-small hide-if-no-js']")
-        slug.click()
-        sleep(3)
-        newslug = driver.find_element_by_xpath("//input[@id='new-post-slug']")
-        newslug.send_keys(Keys.CONTROL + "a")
-        newslug.send_keys(Keys.DELETE)
-        sleep(2)
-        newslug.send_keys(content[1])
-        ok = driver.find_element_by_xpath("//button[@class='save button button-small']")
-        ok.click()
-        sleep(3)
+        body.send_keys(content[2])
+        category_ML = content[3]
 
-        cat = driver.find_element_by_xpath("//div[@id='custom_categorydiv']//button[@class='handlediv']")
-        cat.click()
-        sleep(2)
-        if content[3] == "sport":
-            category = driver.find_element_by_xpath("//input[@id='in-custom_category-18']")
-        elif content[3] == "grants" or content[3] == "parliament" or content[3] == "congress" or content[3] == "EU" or content[3] == "government" or content[3] == "politics":
-            category = driver.find_element_by_xpath("//input[@id='in-custom_category-17']")
-        elif content[3] == "ArtNCulture2":
-            category = driver.find_element_by_xpath("//input[@id='in-custom_category-19']")
-        elif content[3] == "justice" or content[3] == "Crime" or content[3] == "police":
-            category = driver.find_element_by_xpath("//input[@id='in-custom_category-20']")
-        elif content[3] == "society":
-            category = driver.find_element_by_xpath("//input[@id='in-custom_category-16']")
-        elif content[3] == "Economy":
+        if category_ML in ["ECONOMY-BUSINESS"]:
             category = driver.find_element_by_xpath("//input[@id='in-custom_category-14']")
+        elif category_ML in ["POLITICS", "WORLDPOST"]:
+            category = driver.find_element_by_xpath("//input[@id='in-custom_category-17']")
+        elif category_ML in ["JUSTICE"]:
+            category = driver.find_element_by_xpath("//input[@id='in-custom_category-20']")
+        elif category_ML in ["CULTURE"]:
+            category = driver.find_element_by_xpath("//input[@id='in-custom_category-19']")
+        elif category_ML in ["SOCIETY-EDUCATION"]:
+            category = driver.find_element_by_xpath("//input[@id='in-custom_category-16']")
+        elif category_ML in ["SPORT"]:
+            category = driver.find_element_by_xpath("//input[@id='in-custom_category-18']")
         else:
             category = driver.find_element_by_xpath("//input[@id='in-custom_category-23']")
 
-
-        sleep(3)
+        sleep(1)
+        
         try:
             category.click()
         except:
+            cat = driver.find_element_by_xpath("//div[@id='custom_categorydiv']//button[@class='handlediv']")
             cat.click()
+            sleep(1)
             category.click()
         
-        sleep(3)
-        save = driver.find_element_by_xpath("//input[@value='下書きとして保存']")
-        save.click()
+        sleep(1)
+        driver.execute_script("window.scrollTo(0, 0)")
+
+        slug = driver.find_element_by_xpath("//button[@class='edit-slug button button-small hide-if-no-js']")
+        slug.click()
+        sleep(1)
+        newslug = driver.find_element_by_xpath("//input[@id='new-post-slug']")
+        newslug.send_keys(Keys.CONTROL + "a")
+        newslug.send_keys(Keys.DELETE)
+        sleep(1)
+        newslug.send_keys(content[1])
+        ok = driver.find_element_by_xpath( "//button[@class='save button button-small']")
+        ok.click()
+        sleep(1)
+
+
+        
+        #save = driver.find_element_by_xpath( "//input[@value='下書きとして保存']")
+        publish = driver.find_element_by_xpath("//input[@class='button button-primary button-large']")
+        publish.click()
+        #save.click()
         sleep(3)
 
 
